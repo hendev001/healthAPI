67d2a3f70ddc0ae0077ae3212c45c9b52a5c4036
Samuel Henga <divstack03@gmail.com>
2026-04-16 16:43:58 -0500

    Create our controller to match our repo for getting all students and posting the db.

--- Controllers/SchoolCrontroller.cs
+++ Controllers/SchoolCrontroller.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using healthAPI.DTOs;
+using healthAPI.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace healthAPI.Controllers
 {
@@ -7,6 +10,43 @@ namespace healthAPI.Controllers
     [ApiController]
     public class SchoolCrontroller : ControllerBase
     {
-        public SchoolCrontroller() { }       
+        private readonly IStudentRepository studentRepository;
+        public SchoolCrontroller(IStudentRepository studentRepository)
+        {
+            this.studentRepository = studentRepository;
+        }
+
+
+        // api/School/GetAllStudents
+        [HttpGet("GetAllStudents")]
+        public IActionResult Get()
+        {
+            var students = studentRepository.GetAllStudents();
+            return Ok(students);
+        }
+
+
+        //[HttpGet("{id}")]
+        //public IActionResult Get(int id)
+        //{
+        //    var student = studentRepository.GetStudentById(id);
+        //    if (student == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(student);
+        //}
+
+
+        [HttpPost("AddStudent")]
+        public IActionResult Post([FromBody] StudentDto student)
+        {
+             var result = studentRepository.AddStudent(student);
+            if (result == null)
+                {
+                return BadRequest("Failed to add student.");
+            }       
+            return Ok(result);
+        }
     }
 }
