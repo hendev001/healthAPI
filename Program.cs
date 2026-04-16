8da18ce6bf71c27ac976187c0ee597136a985bc2
Samuel Henga <divstack03@gmail.com>
2026-04-16 14:48:20 -0500

    Add dbcontext to the program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using healthAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDbContext>
+    (x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 
 var app = builder.Build();
