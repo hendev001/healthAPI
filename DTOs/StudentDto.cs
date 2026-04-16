f76d7932ccbebad8657480aebf401d3ce1ea9166
Samuel Henga <divstack03@gmail.com>
2026-04-16 16:44:44 -0500

    Add DTO class

---
+++ DTOs/StudentDto.cs
@@ -0,0 +1,13 @@
+﻿namespace healthAPI.DTOs
+{
+    public class StudentDto
+    {
+        public string StudentName { get; set; }
+       = string.Empty;
+        public DateTime DOB { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Email { get; set; }
+        public string SSN { get; set; } = string.Empty;
+    }
+}
