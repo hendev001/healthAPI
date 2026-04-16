0dc7a7741d1d0f92a5beb19b9a07b0543316b5c3
Samuel Henga <divstack03@gmail.com>
2026-04-16 15:05:35 -0500

    Create school controller

---
+++ Controllers/SchoolCrontroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace healthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchoolCrontroller : ControllerBase
+    {
+        public SchoolCrontroller() { }       
+    }
+}
