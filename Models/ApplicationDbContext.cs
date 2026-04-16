fb9e527756c2d2f7f25164a4d070bfd79dbef30d
Samuel Henga <divstack03@gmail.com>
2026-04-16 14:31:34 -0500

    Add project files.

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace healthAPI.Models
+{
+    public class ApplicationDbContext : DbContext 
+    {
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace healthAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        = string.Empty;
+        public DateTime DOB { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Email { get; set; }
+        public string SSN { get; set; } = string.Empty;
+    }
+}
